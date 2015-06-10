a433be8b78ed40e88a55438cb644471fd8f0acaa
Juan Emilio Turk <juanemilioturk@gmail.com>
2015-06-09 22:47:12 -0300

    Fix routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,20 +23,14 @@ Route::resource('offers', 'OffersController');
 Route::resource('products', 'ProductsController');
 
 // Contacto
-Route::get('contacto', 'ContactController@index');
-Route::post('contacto', 'ContactController@store');
+Route::get('contacto', ['as' => 'contact.index', 'uses' => 'ContactController@index']);
+Route::post('contacto', ['as' => 'contact.store', 'uses' => 'ContactController@store']);
 
 // Ficha tecnica
-Route::get('ficha-tecnica', function() {
-    return view('statics/ficha-tecnica.blade.php');
-});
+Route::get('ficha-tecnica', ['as' => 'fichaTecnica', 'uses' => 'StaticsController@ficha']);
 
 // Como se hace su vivienda en 9 pasos
-Route::get('vivienda', function() {
-    return view('statics/vivienda.blade.php');
-});
+Route::get('vivienda', ['as' => 'vivienda', 'uses' => 'StaticsController@vivienda']);
 
 // La empresa
-Route::get('la-empresa', function() {
-    return view('statics/la-empresa.blade.php');
-});
+Route::get('la-empresa', ['as' => 'laEmpresa', 'uses' => 'StaticsController@empresa']);
