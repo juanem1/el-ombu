6f39acc611ad0c52c15158eccf7b5c72d59288c4
Juan Emilio Turk <juanemilioturk@gmail.com>
2015-06-09 22:50:36 -0300

    Vivienda view

--- resources/views/statics/vivienda.blade.php
+++ resources/views/statics/vivienda.blade.php
@@ -1,1 +1,14 @@
-<h1>vivienda</h1>
+@extends('app')
+
+@section('content')
+    <section id="page-title">
+        <div class="container clearfix">
+            <h1>Vivienda</h1>
+            <span>Vea como se construye su próxima vivienda El Ombú en sólo 9 pasos.</span>
+        </div>
+    </section>
+@endsection
+
+@section('scripts')
+    <script type="text/javascript" src="js/functions.js"></script>
+@endsection
