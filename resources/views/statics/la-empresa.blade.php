d734381dc211437bcacd039b6d97d28b2bcbf912
Juan Emilio Turk <juanemilioturk@gmail.com>
2015-06-09 22:50:23 -0300

    La empresa view

--- resources/views/statics/la-empresa.blade.php
+++ resources/views/statics/la-empresa.blade.php
@@ -1,1 +1,14 @@
-<h1>la empresa</h1>
+@extends('app')
+
+@section('content')
+    <section id="page-title">
+        <div class="container clearfix">
+            <h1>La empresa</h1>
+            {{--<span>La fabricación de nuestras Viviendas Americanas se realiza según las siguientes características técnicas.</span>--}}
+        </div>
+    </section>
+@endsection
+
+@section('scripts')
+    <script type="text/javascript" src="js/functions.js"></script>
+@endsection
