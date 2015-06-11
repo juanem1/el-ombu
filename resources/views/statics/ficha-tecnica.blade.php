bb479e252cc0e3a58284be886fea535d6f999642
Juan Emilio Turk <juanemilioturk@gmail.com>
2015-06-11 01:13:55 -0300

    Ficha tecnica page

--- resources/views/statics/ficha-tecnica.blade.php
+++ resources/views/statics/ficha-tecnica.blade.php
@@ -7,6 +7,133 @@
             <span>La fabricación de nuestras Viviendas Americanas se realiza según las siguientes características técnicas.</span>
         </div>
     </section>
+
+    <section id="content">
+        <div class="content-wrap">
+            <div class="container clearfix">
+
+                <div class="col_two_third">
+                    <div data-animate="fadeIn">
+                        <div class="fancy-title title-bottom-border">
+                            <h3>Base de la vivienda:</h3>
+                        </div>
+                        <p>En nuestro sistema de constructivo, necesitamos del contrapiso, base, o platea de la vivienda como elemento indispensable para la instalación. Dejamos la vivienda nivelada la misma lista para que el comprador pueda luego realizarle, una carpeta y luego revestirlo con los cerámicos, alfombra, etc.</p>
+                    </div>
+
+                    <div data-animate="fadeIn">
+                        <div class="fancy-title title-bottom-border">
+                            <h3>Paneles preensamblados:</h3>
+                        </div>
+                        <p>Los paneles son del tipo portante y los fabricamos con tirantería de madera de 25 Mm x 50 Mm, (esto para viviendas de planta baja solamente), con una separación  de 40 cm entre sí. Su parte exterior se reviste con placas cementicias (fibrocemento) Eternit de 6 mm. Interiormente se reviste con placas de yeso del tipo Durlock de 12,5 / 9,5 de espesor. Las paredes divisorias de ambientes son construidos en el mismo tipo de tirantería que los exteriores y revestidos en ambas caras con placas de yeso compactado del tipo Durlock de 12,5 / 9,5 de espesor.</p>
+                    </div>
+
+                    <div data-animate="fadeIn">
+                        <div class="fancy-title title-bottom-border">
+                            <h3>Cielorraso:</h3>
+                        </div>
+                        <p>Construido con estructura interna en madera dura de 1” x 2“ y forrados con madera machihembrada de 1/2 x 4“ todos en madera de pino de 1º calidad.</p>
+                    </div>
+
+                    <div data-animate="fadeIn">
+                        <div class="fancy-title title-bottom-border">
+                            <h3>Cabreadas de techo</h3>
+                        </div>
+                        <p>Las fabricamos con tirantes de madera de 1 x 2 pulgadas. Las exteriores son forradas con placas cementicias Eternit de 6 mm de espesor. Las cabreadas interiores se instalan a 1,20 m de distancia entre sí y son la sustentación para el cielorraso y techo.</p>
+                    </div>
+
+                    <div data-animate="fadeIn">
+                        <div class="fancy-title title-bottom-border">
+                            <h3>Aleros:</h3>
+                        </div>
+                        <p>Todo el perímetro de caída  de la vivienda lucirá un alero de 25 cm. de ancho forrado con madera machihembrada de 12,5 Mm .de espesor.</p>
+                    </div>
+
+                    <div data-animate="fadeIn">
+                        <div class="fancy-title title-bottom-border">
+                            <h3>Techos:</h3>
+                        </div>
+                        <p>Su estructura es de madera de 1" X 2 1/2" pulgadas en paneles desarmables, a dos aguas, la cubierta es de chapa de hierro aluminizado, LA MISMA ES DE CALIBRE 25 (antigranizo),  chapa sinusoidal ó trapezoidal pintada u opcional con tejas plásticas.</p>
+                    </div>
+
+                    <div data-animate="fadeIn">
+                        <div class="fancy-title title-bottom-border">
+                            <h3>Altura:</h3>
+                        </div>
+                        <p>La altura que tendrá vivienda entre el nivel del piso terminado y el cielorraso será de 2,40 Mts.</p>
+                    </div>
+
+                    <div data-animate="fadeIn">
+                        <div class="fancy-title title-bottom-border">
+                            <h3>Puertas:</h3>
+                        </div>
+                        <p>La exterior al frente de chapa de hierro doble inyectadas en poliuretano con cerradura de seguridad. La puerta de acceso a la parte trasera de la casa ( si corresponde que la lleve) es de chapa también. Las puertas interiores son de tipo placas forradas en madera de pino, con marcos metálicos o de madera.</p>
+                    </div>
+
+                    <div data-animate="fadeIn">
+                        <div class="fancy-title title-bottom-border">
+                            <h3>Ventanas:</h3>
+                        </div>
+                        <p>Las mismas son de aluminio de cómo estándar llevan de 1,10 x 1,20 y 1,50 x 1,10 con rejas (opcional en hierro con postigones o madera). En baño se colocara ventiluz tipo balancín de aluminio de 0.25 x 0.40 mts.</p>
+                    </div>
+
+                    <div data-animate="fadeIn">
+                        <div class="fancy-title title-bottom-border">
+                            <h3>Instalaciones:</h3>
+                        </div>
+                        <ul class="leftmargin-sm">
+                            <li>Agua: Las instalaciones para agua fría y caliente serán embutidas y se realizarán con caños de polipropileno de primera calidad, todo en 1/2 “ (opcional con caños de termofusión)</li>
+                            <li>Gas: La instalación es embutida y se efectúa con caño epoxi de 3/4, se entrega con llaves de corte y boca para cocina y calefón o temo tanque, como opcional se instala boca para calefactor (opcional con caños de termofusión)</li>
+                            <li>Luz: Se entregan todas las cañerías instaladas y con las bocas en pared y techo, sin cableado ni artefactos.</li>
+                        </ul>
+                    </div>
+                    <div data-animate="fadeIn">
+                        <div class="fancy-title title-bottom-border">
+                            <h3>Baño:</h3>
+                        </div>
+                        <p>Se fabrica igual que las otras dependencias, solo que aquí se forraran las paredes interiores en el mismo fibrocemento, especial para lugares con humedad o traslado de aguas.</p>
+                    </div>
+
+                    <div data-animate="fadeIn">
+                        <div class="fancy-title title-bottom-border">
+                            <h3>Terminaciones:</h3>
+                        </div>
+                        <p>Se entrega con todos los contramarcos en aberturas y con terminación en ángulo en esquinas de techo, la casa se entrega totalmente cerrada con vidrios colocados, todo le queda listo para pintar.</p>
+                    </div>
+
+                    <div data-animate="fadeIn">
+                        <div class="fancy-title title-bottom-border">
+                            <h3>Aislacion:</h3>
+                        </div>
+                        <p>Todos los paneles y el cielorraso llevan un aislamiento termo acústica realizada con telgopor en planchas o con aislamiento detelgopor doble en lugares de alta temperatura (opcional con lana de vidrio).</p>
+                    </div>
+                </div>
+                <div class="col_one_third col_last">
+                    <div class="center">
+                        <img src="/images/tecnica/fondotecnica.jpg" alt="Fondo caracteristicas tecnicas" />
+                    </div>
+
+                    <div class="divider divider-rounded divider-center"><i class="icon-home"></i></div>
+
+                    <div>
+                        <h3>Que le queda por hacer despues que le instalamos su vivienda para que la misma sea habitable</h3>
+                        <ul class="leftmargin-sm">
+                            <li>Realizar el contrapiso</li>
+                            <li>instalar los caños de cloacas</li>
+                            <li>instalar el cableado eléctrico interno</li>
+                            <li>y colocar los cerámicos</li>
+                        </ul>
+                    </div>
+
+                    <div class="divider divider-rounded divider-center"><i class="icon-home"></i></div>
+
+                    <div class="center">
+                        <img src="/images/tecnica/marcas.jpg" alt="Marcas usadas para la construccion" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
 @endsection
 
 @section('scripts')
