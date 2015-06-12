c7c879e2f0ba4439f352a805e8ab8365d630db40
Juan Emilio Turk <juanemilioturk@gmail.com>
2015-06-12 19:51:08 -0300

    Construccion paso a paso page

--- resources/views/statics/vivienda.blade.php
+++ resources/views/statics/vivienda.blade.php
@@ -7,6 +7,22 @@
             <span>Vea como se construye su próxima vivienda El Ombú en sólo 9 pasos.</span>
         </div>
     </section>
+
+    <section id="content">
+        <div class="content-wrap">
+            <div class="container clearfix">
+
+                <object type="application/x-shockwave-flash" data="/movies/pasoapaso.swf" width="800" height="500">
+                    <param name="movie" value="/movies/pasoapaso.swf" />
+                    <param name="quality" value="high"/>
+                </object>
+
+            </div>
+        </div>
+    </section>
+
+
+
 @endsection
 
 @section('scripts')
