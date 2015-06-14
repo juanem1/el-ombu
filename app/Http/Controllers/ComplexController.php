964cd8abbdfe2c832a40d2a7afd2e6814255e48b
Juan Emilio Turk <juanemilioturk@gmail.com>
2015-06-14 18:41:41 -0300

    Pull data to build complex page

--- app/Http/Controllers/ComplexController.php
+++ app/Http/Controllers/ComplexController.php
@@ -1,8 +1,10 @@
 <?php namespace App\Http\Controllers;
 
+use App\Complex;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Images;
 use Illuminate\Http\Request;
 
 class ComplexController extends Controller {
@@ -47,6 +49,20 @@ class ComplexController extends Controller {
 	{
 		//
 	}
+
+    /**
+     * Display the specified from its slug
+     *
+     * @param  String $slug
+     * @return Response
+     */
+    public function view($slug)
+    {
+        $complex = Complex::whereSlug($slug)->first();
+        $images = Images::where('gallery_id', $complex->gallery_id)->get()->toArray();
+        //dd($images);
+        return view('complex.view', compact('complex', 'images'));
+    }
 
 	/**
 	 * Show the form for editing the specified resource.
