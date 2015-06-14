df2a17031617cd56248cd5f3a2c6e74168865e65
Juan Emilio Turk <juanemilioturk@gmail.com>
2015-06-14 18:39:52 -0300

    Add la empresa page

--- resources/views/statics/la-empresa.blade.php
+++ resources/views/statics/la-empresa.blade.php
@@ -7,8 +7,29 @@
             {{--<span>La fabricación de nuestras Viviendas Americanas se realiza según las siguientes características técnicas.</span>--}}
         </div>
     </section>
+
+    <section id="content">
+        <div class="content-wrap">
+            <div class="container clearfix">
+
+                <div class="col_two_third">
+                    <p><strong>VIVIENDAS EL OMBU</strong> nace en una época de grandes cambios y volatilidad en el país, con el fin de que todas aquellas personas, inversores y/o compradores tuvieran en la ciudad de Córdoba alternativas distintas de construir. Se caracteriza fundamentalmente por, trabajar activa y creativamente en el diseño, desarrollo integral, construcción y comercialización de viviendas industrializadas de primera calidad.</p>
+                    <p>También por estar cerca del INVERSOR, por cuidar del mismo, por hacer que este tenga su mejor opción en el momento de resguardar sus ahorros y elegir la alternativa más conveniente al momento de decidir.</p>
+                    <p>Nuestra empresa se caracteriza principalmente por estar integrada en todos los aspectos constructivos desde la inversión inicial hasta la comercialización de las unidades construidas, teniendo un gran abanico de terminaciones y revestimientos exteriores, muchos de ellos exclusivos en el mercado nacional.</p>
+                    <p>Esto hace que en <strong>VIVIENDAS EL OMBU</strong> se logre disminuir los costos de edificación y trasladar esa diferencia a un mejor precio y sin disminuir la calidad de nuestras viviendas. Contamos con la infraestructura necesaria para llevar a cabo distintas construcciones, tanto desde el punto de vista material como humano. Nuestra experiencia en el mercado marca en la ciudad de Córdoba, la tendencia de una Empresa con estilo propio, calidad, seriedad, confianza y responsabilidad.</p>
+                    <p>Los equipos interdisciplinarios que trabajan en materia Técnica (Arquitectos, Ingenieros, Contratistas, Instaladores etc.) Administrativa, Comercial y Financiera.</p>
+                </div>
+                <div class="col_one_third col_last">
+                    <div class="center">
+                        <img src="/images/tecnica/fondotecnica.jpg" alt="Fondo la empresa" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
 @endsection
 
 @section('scripts')
-    <script type="text/javascript" src="js/functions.js"></script>
+    <script type="text/javascript" src="/js/functions.js"></script>
 @endsection
