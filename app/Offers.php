eee8a88c46e4093c0edb486d7299ff173eab8fed
Juan Emilio Turk <juanemilioturk@gmail.com>
2015-07-06 20:06:32 -0300

    Add fillable fields to offers model

--- app/Offers.php
+++ app/Offers.php
@@ -4,6 +4,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Offers extends Model {
 
+    protected $fillable = [
+        'title',
+        'price',
+        'product_id'
+    ];
+
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasOne
      */
