bfba9cd776acec385680465ca4091a582b2934e6
Juan Emilio Turk <juanemilioturk@gmail.com>
2015-08-17 14:41:39 -0300

    Add header to the products page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layout/back')
 
 @section('content')
+    <section id="page-title" class="page-title-mini">
+        <div class="container clearfix">
+            <h1>Administrar ofertas</h1>
+        </div>
+    </section>
+
     <section id="content">
         <div class="content-wrap">
             <div class="container clearfix">
