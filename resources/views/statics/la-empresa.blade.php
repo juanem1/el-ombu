568f788f1a279bd08871fa7e6a9caf275b7c911f
Juan Emilio Turk <juanemilioturk@gmail.com>
2015-08-23 23:14:36 -0300

    Update "la empresa" page

--- resources/views/statics/la-empresa.blade.php
+++ resources/views/statics/la-empresa.blade.php
@@ -3,8 +3,7 @@
 @section('content')
     <section id="page-title">
         <div class="container clearfix">
-            <h1>La empresa</h1>
-            {{--<span>La fabricación de nuestras Viviendas Americanas se realiza según las siguientes características técnicas.</span>--}}
+            <h1>NUESTRA EMPRESA</h1>
         </div>
     </section>
 
@@ -18,6 +17,20 @@
                     <p>Nuestra empresa se caracteriza principalmente por estar integrada en todos los aspectos constructivos desde la inversión inicial hasta la comercialización de las unidades construidas, teniendo un gran abanico de terminaciones y revestimientos exteriores, muchos de ellos exclusivos en el mercado nacional.</p>
                     <p>Esto hace que en <strong>VIVIENDAS EL OMBU</strong> se logre disminuir los costos de edificación y trasladar esa diferencia a un mejor precio y sin disminuir la calidad de nuestras viviendas. Contamos con la infraestructura necesaria para llevar a cabo distintas construcciones, tanto desde el punto de vista material como humano. Nuestra experiencia en el mercado marca en la ciudad de Córdoba, la tendencia de una Empresa con estilo propio, calidad, seriedad, confianza y responsabilidad.</p>
                     <p>Los equipos interdisciplinarios que trabajan en materia Técnica (Arquitectos, Ingenieros, Contratistas, Instaladores etc.) Administrativa, Comercial y Financiera.</p>
+
+                    <h3>MISION</h3>
+                    <p>Satisfacer las necesidades de entretenimiento, consumo, vivienda e inversión de la población, generando puestos de trabajo dignos y actuando con responsabilidad ante la comunidad y el medio ambiente.</p>
+
+                    <h3>VISION</h3>
+                    <p>Ser uno de los principales propulsores del desarrollo urbanístico en el interior del país, adaptando la oferta de productos y servicios a los cambios en la demanda de nuestros clientes, mediante la incorporación de nuevas tecnologías.</p>
+                    <p>Los pilares de <strong>VIVIENDAS EL OMBU</strong> son los Valores, que suponen la guía de nuestro comportamiento en el ámbito empresarial. Ellos son:</p>
+                    <ul>
+                        <li><strong>Honestidad:</strong> Promovemos la transparencia en nuestras relaciones, rechazando las prácticas injustas orientadas a obtener una ventaja particular a favor de uno mismo.</li>
+                        <li><strong>Solidaridad:</strong> Promovemos la Solidaridad con los clientes, proveedores, compañeros y la comunidad, favoreciendo la disposición al trabajo en equipo y la integración.</li>
+                        <li><strong>Respeto:</strong> Promovemos el Respeto en las relaciones interpersonales, valorando los intereses, posiciones y necesidades de nuestros compañeros y clientes.</li>
+                        <li><strong>Servicio:</strong> Promovemos la actitud de Servicio, estando siempre predispuesto a brindarse fidedignamente a los colaboradores y clientes, utilizando el Modelo de Atención al Cliente con estos últimos.</li>
+                        <li><strong>Lealtad:</strong> Promovemos la Lealtad, es decir, la fidelidad que un colaborador tiene con la empresa, la visión y misión que la misma persigue.</li>
+                    </ul>
                 </div>
                 <div class="col_one_third col_last">
                     <div class="center">
