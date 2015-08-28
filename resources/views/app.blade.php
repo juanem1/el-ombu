6c132836974b784c5e4ce75ecc93ff5b9ceb0ad2
Juan Emilio Turk <juanemilioturk@gmail.com>
2015-08-27 21:04:38 -0300

    Add vendor versioned css file Add app versioned js file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,14 +3,15 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="author" content="iBox design studio" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <!-- Favicon -->
     <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
     <title>Viviendas Americanas El Ombú | Empresa Constructora S.R.L. | Una raza que crece fuerte</title>
 
     <!-- Stylesheets ============================================= -->
     <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+    <link href="{{ elixir('css/vendor.css') }}" rel="stylesheet" />
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
     <!--[if lt IE 9]>
     <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
@@ -27,9 +28,7 @@
 <div id="gotoTop" class="icon-angle-up"></div>
 
 <!-- Footer Scripts ============================================= -->
-<script type="text/javascript" src="/js/jquery.js"></script>
-<script type="text/javascript" src="/js/plugins.js"></script>
-<script type="text/javascript" src="/js/functions.js"></script>
+<script type="text/javascript" src="{{ elixir('js/app.js') }}"></script>
 @yield('scripts')
 
 </body>
