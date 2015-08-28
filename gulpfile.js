63792f5515ca47c28d19d57123f0fb5b4a1def95
Juan Emilio Turk <juanemilioturk@gmail.com>
2015-08-27 21:05:15 -0300

    Create tasks for mix, minify and versioning css and js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,29 @@
 var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
-    mix
-        .less('app.less')
-        .version('css/app.css');
+
+    // Vendor styles
+    mix.styles(['font-awesome.min.css', 'simplemde.min.css'],
+        './public/css/vendor.css'
+    );
+
+    // App styles
+    mix.less('app.less');
+
+    // Vendor scripts
+    mix.scripts(
+    	[
+    		'jquery.js', 
+    		'plugins.js',
+    		'functions.js',
+    		'jquery.camera.js',
+    		'simplemde.min.js'
+    	],
+        './public/js/app.js'
+    );
+
+    // Versioning files
+    mix.version(['css/vendor.css', 'css/app.css', 'js/app.js']);
+
+
 });
