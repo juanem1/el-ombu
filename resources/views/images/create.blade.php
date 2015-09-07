cd398e9ff83cd8a4d0c15c2f4f65cbdf708a934a
Juan Emilio Turk <juanemilioturk@gmail.com>
2015-09-06 22:26:49 -0300

    update message in upload image form

--- resources/views/images/create.blade.php
+++ resources/views/images/create.blade.php
@@ -10,6 +10,15 @@
     <section id="content">
         <div class="content-wrap">
             <div class="container clearfix">
+
+                <div class="style-msg infomsg">
+                    <div class="sb-msg">
+                        <i class="icon-info-sign"></i>
+                        El tamaño maximo de cada imagen no puede superar 1MB.
+                        Recomendamos usar esta herramienta de compresion: <a href="https://compressor.io/" target="_blank">https://compressor.io/</a>
+                    </div>
+                </div>
+
                 {!! form($form) !!}
             </div>
         </div>
--- resources/views/images/edit.blade.php
+++ resources/views/images/edit.blade.php
@@ -17,6 +17,14 @@
 
                 <hr/>
 
+                <div class="style-msg infomsg">
+                    <div class="sb-msg">
+                        <i class="icon-info-sign"></i>
+                        El tamaño maximo de cada imagen no puede superar 1MB.
+                        Recomendamos usar esta herramienta de compresion: <a href="https://compressor.io/" target="_blank">https://compressor.io/</a>
+                    </div>
+                </div>
+
                 {!! form($form) !!}
 
             </div>
